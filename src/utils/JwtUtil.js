60d2f0ac1de9f0f962bf7ba173c1f14645b66943
dnh <rootdnh@gmail.com>
2023-09-04 13:34:15 -0300

    refactor: jwtUtil

--- src/utils/JwtUtil.js
+++ src/utils/JwtUtil.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken";
+import HandleError from "../error/handleError";
 
 class JwtUtil {
  constructor() {
@@ -8,23 +9,26 @@ class JwtUtil {
 
  generate(payload) {
   try {
-    const token =  jwt.sign(payload, this.secret, { expiresIn: this.expiresTokenIn });
-    return token || null  
+   const token = jwt.sign(payload, this.secret, {
+    expiresIn: this.expiresTokenIn
+   });
+   if(!token){
+    throw new HandleError("Error when trying to generate a token", 500)
+   }
+   return token;
   } catch (error) {
-    console.error(error)
-    return null;
+    if(error instanceof HandleError) throw error;
+   throw new HandleError("Error when trying to generate a token", 500, error);
   }
  }
 
  verify(token) {
   try {
    const isValid = jwt.verify(token, this.secret);
-   return !!isValid
+   return !!isValid;
   } catch (error) {
-   console.error(error);
-   return null;
+    throw new HandleError("Error then trying to verify a token", 500, error)
   }
  }
- 
 }
 export default new JwtUtil();
